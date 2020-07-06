9b06c86eb79c748e0abc4f722be8824b7b114cf7
dancreppel <creppel@usc.edu>
2020-07-06 13:37:43 -0700

    Create index, show, and create routes for tracks

---
+++ routes/api/tracks.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const Track = require('../../models/Track');
+const passport = require("passport");
+
+// test
+router.get("/test", (req, res) => res.json({ msg: "This is the tracks route" }));
+
+// tracks index: return all tracks
+router.get("/", (req, res) => {
+  Track
+    .find()
+    .sort({ date: -1 })
+    .then(tracks => res.json(tracks))
+    .catch(err => res.status(400).json(err))
+});
+
+// tracks show
+router.get("/user/:user_id", (req, res) => {
+  Track
+    .findById(req.params.user_id)
+    .sort({ date: -1 })
+    .then(tracks => res.json(tracks))
+    .catch(err => res.status(400).json(err))
+});
+
+// tracks create
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  // attach user to track
+  const track = new Track({
+    user: req.user.id
+  });
+
+  // populate blocks
+  track.blocks = req.body.blocks.map(block => block.id);
+  track.save();
+  res.json('Created new track.');
+});
+
+module.exports = router;
