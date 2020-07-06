415b9d8b184aef08cff8f943afe459c3dcf0027c
Daniel Chau <danchau88@gmail.com>
2020-07-06 11:57:06 -0700

    bootstrap normal css works

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,11 +1,13 @@
 import React from "react";
+import './mainpage.scss';
+import
 
 //what users will see when they land on the home page
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>MERNblocks</h1>
+      <div className='main-page'>
+        <h1 className='title'>MERNblocks</h1>
         <footer>Copyright &copy; 2020 Daniel Group</footer>
       </div>
     );
