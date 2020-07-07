5eda49c01e9ed7bded6a69d75d5a8173b694493c
Daniel Yee <danjamesyee@gmail.com>
2020-07-07 14:52:20 -0700

    Adds title to track

--- frontend/src/components/tracks/tracks.js
+++ frontend/src/components/tracks/tracks.js
@@ -8,7 +8,7 @@ class Tracks extends React.Component {
     this.playNote = this.playNote.bind(this);
     this.sleep = this.sleep.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {};
+    this.state = { title: "" };
     this.state.track = [];
     this.state.notes = [];
   }
@@ -17,10 +17,17 @@ class Tracks extends React.Component {
     this.props.fetchBlocks();
   }
 
+  update(field) {
+    return (e) =>
+      this.setState({
+        [field]: e.currentTarget.value,
+      });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     debugger;
-    this.props.makeTrack(this.state.track);
+    this.props.makeTrack({ title: this.state.title, blocks: this.state.track });
   }
 
   addNoteToTrack(note, length, block) {
@@ -85,166 +92,174 @@ class Tracks extends React.Component {
     return (
       <div>
         <h1>MERNblocks</h1>
-        <ul>
-          {notes.map((note, i) => (
-            <div key={i}>{note}</div>
-          ))}
-        </ul>
-        <button onClick={() => this.playNote()}>Play</button>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title
+            <br />
+            <input
+              type="text"
+              value={this.state.title}
+              onChange={this.update("title")}
+            />
+          </label>
+          <ul>
+            {notes.map((note, i) => (
+              <div key={i}>{note}</div>
+            ))}
+          </ul>
+          <button onClick={() => this.playNote()}>Play</button>
+          <h3>Quarter Notes</h3>
+          <button
+            onClick={() => this.addNoteToTrack("C4", "4n", blocks[0])}
+            onMouseEnter={() => synth.triggerAttackRelease("C4", "4n")}
+          >
+            C4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("D4", "4n", blocks[1])}
+            onMouseEnter={() => synth.triggerAttackRelease("D4", "4n")}
+          >
+            D4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("E4", "4n", blocks[3])}
+            onMouseEnter={() => synth.triggerAttackRelease("E4", "4n")}
+          >
+            E4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("F4", "4n", blocks[2])}
+            onMouseEnter={() => synth.triggerAttackRelease("F4", "4n")}
+          >
+            F4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("G4", "4n", blocks[4])}
+            onMouseEnter={() => synth.triggerAttackRelease("G4", "4n")}
+          >
+            G4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("A4", "4n", blocks[8])}
+            onMouseEnter={() => synth.triggerAttackRelease("A4", "4n")}
+          >
+            A4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("B4", "4n", blocks[5])}
+            onMouseEnter={() => synth.triggerAttackRelease("B4", "4n")}
+          >
+            B4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("C5", "4n", blocks[6])}
+            onMouseEnter={() => synth.triggerAttackRelease("C5", "4n")}
+          >
+            C5
+          </button>
+          <h3>Eighth notes</h3>
+          <button
+            onClick={() => this.addNoteToTrack("C4", "8n", blocks[7])}
+            onMouseEnter={() => synth.triggerAttackRelease("C4", "8n")}
+          >
+            C4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("D4", "8n", blocks[10])}
+            onMouseEnter={() => synth.triggerAttackRelease("D4", "8n")}
+          >
+            D4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("E4", "8n", blocks[9])}
+            onMouseEnter={() => synth.triggerAttackRelease("E4", "8n")}
+          >
+            E4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("F4", "8n", blocks[12])}
+            onMouseEnter={() => synth.triggerAttackRelease("F4", "8n")}
+          >
+            F4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("G4", "8n", blocks[13])}
+            onMouseEnter={() => synth.triggerAttackRelease("G4", "8n")}
+          >
+            G4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("A4", "8n", blocks[11])}
+            onMouseEnter={() => synth.triggerAttackRelease("A4", "8n")}
+          >
+            A4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("B4", "8n", blocks[17])}
+            onMouseEnter={() => synth.triggerAttackRelease("B4", "8n")}
+          >
+            B4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("C5", "8n", blocks[16])}
+            onMouseEnter={() => synth.triggerAttackRelease("C5", "8n")}
+          >
+            C5
+          </button>
+          <h3>Sixteenth notes</h3>
+          <button
+            onClick={() => this.addNoteToTrack("C4", "16n", blocks[15])}
+            onMouseEnter={() => synth.triggerAttackRelease("C4", "16n")}
+          >
+            C4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("D4", "16n", blocks[18])}
+            onMouseEnter={() => synth.triggerAttackRelease("D4", "16n")}
+          >
+            D4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("E4", "16n", blocks[14])}
+            onMouseEnter={() => synth.triggerAttackRelease("E4", "16n")}
+          >
+            E4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("F4", "16n", blocks[20])}
+            onMouseEnter={() => synth.triggerAttackRelease("F4", "16n")}
+          >
+            F4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("G4", "16n", blocks[19])}
+            onMouseEnter={() => synth.triggerAttackRelease("G4", "16n")}
+          >
+            G4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("A4", "16n", blocks[21])}
+            onMouseEnter={() => synth.triggerAttackRelease("A4", "16n")}
+          >
+            A4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("B4", "16n", blocks[23])}
+            onMouseEnter={() => synth.triggerAttackRelease("B4", "16n")}
+          >
+            B4
+          </button>
+          <button
+            onClick={() => this.addNoteToTrack("C5", "16n", blocks[22])}
+            onMouseEnter={() => synth.triggerAttackRelease("C5", "16n")}
+          >
+            C5
+          </button>
+          <br />
+          <br />
 
-        <h3>Quarter Notes</h3>
-        <button
-          onClick={() => this.addNoteToTrack("C4", "4n", blocks[0])}
-          onMouseEnter={() => synth.triggerAttackRelease("C4", "4n")}
-        >
-          C4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("D4", "4n", blocks[1])}
-          onMouseEnter={() => synth.triggerAttackRelease("D4", "4n")}
-        >
-          D4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("E4", "4n", blocks[3])}
-          onMouseEnter={() => synth.triggerAttackRelease("E4", "4n")}
-        >
-          E4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("F4", "4n", blocks[2])}
-          onMouseEnter={() => synth.triggerAttackRelease("F4", "4n")}
-        >
-          F4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("G4", "4n", blocks[4])}
-          onMouseEnter={() => synth.triggerAttackRelease("G4", "4n")}
-        >
-          G4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("A4", "4n", blocks[8])}
-          onMouseEnter={() => synth.triggerAttackRelease("A4", "4n")}
-        >
-          A4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("B4", "4n", blocks[5])}
-          onMouseEnter={() => synth.triggerAttackRelease("B4", "4n")}
-        >
-          B4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("C5", "4n", blocks[6])}
-          onMouseEnter={() => synth.triggerAttackRelease("C5", "4n")}
-        >
-          C5
-        </button>
-
-        <h3>Eighth notes</h3>
-        <button
-          onClick={() => this.addNoteToTrack("C4", "8n", blocks[7])}
-          onMouseEnter={() => synth.triggerAttackRelease("C4", "8n")}
-        >
-          C4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("D4", "8n", blocks[10])}
-          onMouseEnter={() => synth.triggerAttackRelease("D4", "8n")}
-        >
-          D4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("E4", "8n", blocks[9])}
-          onMouseEnter={() => synth.triggerAttackRelease("E4", "8n")}
-        >
-          E4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("F4", "8n", blocks[12])}
-          onMouseEnter={() => synth.triggerAttackRelease("F4", "8n")}
-        >
-          F4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("G4", "8n", blocks[13])}
-          onMouseEnter={() => synth.triggerAttackRelease("G4", "8n")}
-        >
-          G4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("A4", "8n", blocks[11])}
-          onMouseEnter={() => synth.triggerAttackRelease("A4", "8n")}
-        >
-          A4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("B4", "8n", blocks[17])}
-          onMouseEnter={() => synth.triggerAttackRelease("B4", "8n")}
-        >
-          B4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("C5", "8n", blocks[16])}
-          onMouseEnter={() => synth.triggerAttackRelease("C5", "8n")}
-        >
-          C5
-        </button>
-
-        <h3>Sixteenth notes</h3>
-        <button
-          onClick={() => this.addNoteToTrack("C4", "16n", blocks[15])}
-          onMouseEnter={() => synth.triggerAttackRelease("C4", "16n")}
-        >
-          C4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("D4", "16n", blocks[18])}
-          onMouseEnter={() => synth.triggerAttackRelease("D4", "16n")}
-        >
-          D4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("E4", "16n", blocks[14])}
-          onMouseEnter={() => synth.triggerAttackRelease("E4", "16n")}
-        >
-          E4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("F4", "16n", blocks[20])}
-          onMouseEnter={() => synth.triggerAttackRelease("F4", "16n")}
-        >
-          F4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("G4", "16n", blocks[19])}
-          onMouseEnter={() => synth.triggerAttackRelease("G4", "16n")}
-        >
-          G4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("A4", "16n", blocks[21])}
-          onMouseEnter={() => synth.triggerAttackRelease("A4", "16n")}
-        >
-          A4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("B4", "16n", blocks[23])}
-          onMouseEnter={() => synth.triggerAttackRelease("B4", "16n")}
-        >
-          B4
-        </button>
-        <button
-          onClick={() => this.addNoteToTrack("C5", "16n", blocks[22])}
-          onMouseEnter={() => synth.triggerAttackRelease("C5", "16n")}
-        >
-          C5
-        </button>
-
-        <button type="submit" onClick={this.handleSubmit}>
-          Submit
-        </button>
+          <input type="submit" value="Save track" />
+        </form>
         <footer>Copyright &copy; 2020 Daniel Group</footer>
       </div>
     );
