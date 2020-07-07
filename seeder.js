c3c7a0cd28a18ad97c848ce9067f0ae2476b6eaa
dancreppel <creppel@usc.edu>
2020-07-06 21:36:26 -0700

    Create seeder for mongoDB

---
+++ seeder.js
@@ -0,0 +1,135 @@
+const seeder = require('mongoose-seed');
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+seeder.connect(db, () => {
+
+  // Load Mongoose models
+  seeder.loadModels([
+    './models/User.js',
+    './models/Block.js'
+  ]);
+
+  // Clear specified collections
+  seeder.clearModels(['User', 'Block'], () => {
+
+    // Callback to poplulate DB once models are cleared
+    seeder.populateModels(data, () => {
+      seeder.disconnect();
+    });
+  });
+});
+
+const data = [
+  {
+    model: "User",
+    documents: [
+      {
+        handle: "demouser",
+        email: "demouser@gmail.com",
+        password: "password",
+      },
+    ],
+  },
+  {
+    model: "Block",
+    documents: [
+      {
+        note: "C4",
+        duration: "4n",
+      },
+      {
+        note: "D4",
+        duration: "4n",
+      },
+      {
+        note: "E4",
+        duration: "4n",
+      },
+      {
+        note: "F4",
+        duration: "4n",
+      },
+      {
+        note: "G4",
+        duration: "4n",
+      },
+      {
+        note: "A4",
+        duration: "4n",
+      },
+      {
+        note: "B4",
+        duration: "4n",
+      },
+      {
+        note: "C5",
+        duration: "4n",
+      },
+      {
+        note: "C4",
+        duration: "8n",
+      },
+      {
+        note: "D4",
+        duration: "8n",
+      },
+      {
+        note: "E4",
+        duration: "8n",
+      },
+      {
+        note: "F4",
+        duration: "8n",
+      },
+      {
+        note: "G4",
+        duration: "8n",
+      },
+      {
+        note: "A4",
+        duration: "8n",
+      },
+      {
+        note: "B4",
+        duration: "8n",
+      },
+      {
+        note: "C5",
+        duration: "8n",
+      },
+      {
+        note: "C4",
+        duration: "16n",
+      },
+      {
+        note: "D4",
+        duration: "16n",
+      },
+      {
+        note: "E4",
+        duration: "16n",
+      },
+      {
+        note: "F4",
+        duration: "16n",
+      },
+      {
+        note: "G4",
+        duration: "16n",
+      },
+      {
+        note: "A4",
+        duration: "16n",
+      },
+      {
+        note: "B4",
+        duration: "16n",
+      },
+      {
+        note: "C5",
+        duration: "16n",
+      },
+    ],
+  },
+];
