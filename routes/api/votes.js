914adf9218f4de5d48cd979b12be6eda49f1f633
dancreppel <creppel@usc.edu>
2020-07-07 15:18:22 -0700

    Cleanup imports

--- routes/api/votes.js
+++ routes/api/votes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Vote = require("../../models/Vote");
-const Track = require("../../models/Track");
 const passport = require("passport");
 
 // test
