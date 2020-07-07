97ae652e273bea84f89a65cffe62e1a6503ec1a8
Daniel Yee <danjamesyee@gmail.com>
2020-07-06 21:28:47 -0700

    Fixes package.json

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,25 +1,22 @@
 import React from "react";
 
-import './mainpage.scss';
-import { Container, Row, Col, Toast } from 'react-bootstrap'
+import "./mainpage.scss";
+import { Container, Row, Col, Toast } from "react-bootstrap";
 
 //what users will see when they land on the home page
 class MainPage extends React.Component {
   render() {
-    const synth = new Tone.Synth().toMaster();
     // debugger;
 
-
     return (
-
-      
-        
-      <div className='main-page'>
+      <div className="main-page">
         <Container>
           <Row>
             <Toast>
               <Toast.Header>
-                <strong><h1 className='title'>MERNblocks</h1></strong>
+                <strong>
+                  <h1 className="title">MERNblocks</h1>
+                </strong>
                 <small>Checked out</small>
               </Toast.Header>
               <Toast.Body>
@@ -31,8 +28,6 @@ class MainPage extends React.Component {
 
         <footer>Copyright &copy; 2020 Daniel Group</footer>
       </div>
-        
-      
     );
   }
 }
