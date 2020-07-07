9a8a0a7e7405b4a21be122d592e26d7766f9b37c
dancreppel <creppel@usc.edu>
2020-07-07 09:25:40 -0700

    Merge

--- seeder.js
+++ seeder.js
@@ -22,11 +22,7 @@ seeder.connect(db, () => {
 
 // bcrypt demo user password
 const salt = bcrypt.genSaltSync(10);
-<<<<<<< HEAD
-const hash = bcrypt.hashSync('password', salt);
-=======
 const hash = bcrypt.hashSync("password", salt);
->>>>>>> b59b65ab84d1edab2a5ef0e9f3f2b9c75335549e
 
 // Populate demo user and block templates
 const data = [
@@ -36,13 +32,8 @@ const data = [
       {
         handle: "demouser",
         email: "demouser@gmail.com",
-<<<<<<< HEAD
-        password: hash
-      }
-=======
         password: hash,
       },
->>>>>>> b59b65ab84d1edab2a5ef0e9f3f2b9c75335549e
     ],
   },
   {
@@ -50,105 +41,6 @@ const data = [
     documents: [
       {
         note: "C4",
-<<<<<<< HEAD
-        duration: "4n"
-      },
-      {
-        note: "D4",
-        duration: "4n"
-      },
-      {
-        note: "E4",
-        duration: "4n"
-      },
-      {
-        note: "F4",
-        duration: "4n"
-      },
-      {
-        note: "G4",
-        duration: "4n"
-      },
-      {
-        note: "A4",
-        duration: "4n"
-      },
-      {
-        note: "B4",
-        duration: "4n"
-      },
-      {
-        note: "C5",
-        duration: "4n"
-      },
-      {
-        note: "C4",
-        duration: "8n"
-      },
-      {
-        note: "D4",
-        duration: "8n"
-      },
-      {
-        note: "E4",
-        duration: "8n"
-      },
-      {
-        note: "F4",
-        duration: "8n"
-      },
-      {
-        note: "G4",
-        duration: "8n"
-      },
-      {
-        note: "A4",
-        duration: "8n"
-      },
-      {
-        note: "B4",
-        duration: "8n"
-      },
-      {
-        note: "C5",
-        duration: "8n"
-      },
-      {
-        note: "C4",
-        duration: "16n"
-      },
-      {
-        note: "D4",
-        duration: "16n"
-      },
-      {
-        note: "E4",
-        duration: "16n"
-      },
-      {
-        note: "F4",
-        duration: "16n"
-      },
-      {
-        note: "G4",
-        duration: "16n"
-      },
-      {
-        note: "A4",
-        duration: "16n"
-      },
-      {
-        note: "B4",
-        duration: "16n"
-      },
-      {
-        note: "C5",
-        duration: "16n"
-      }
-    ],
-  },
-];
-=======
         duration: "4n",
       },
       {
@@ -246,4 +138,3 @@ const data = [
     ],
   },
 ];
->>>>>>> b59b65ab84d1edab2a5ef0e9f3f2b9c75335549e
