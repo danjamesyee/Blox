e594e6610a30d595d6be1333d3496ef5895148fe
dancreppel <creppel@usc.edu>
2020-07-07 11:15:39 -0700

    Grab vote for specific track

--- routes/api/votes.js
+++ routes/api/votes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Vote = require("../../models/Vote");
+const Track = require("../../models/Track");
 
 // test
 router.get("/test", (req, res) => res.json({ msg: "This is the votes route" }));
@@ -13,5 +14,12 @@ router.get('/', (req, res) => {
     .catch(err => res.status(400).json(err))
 })
 
+// get vote for specific track
+router.get('/track/:track_id', (req, res) => {
+  Track
+    .findById(req.params.track_id)
+    .then(track => res.json(track))
+    .catch(err => res.status(400).json(err));
+});
 
 module.exports = router;
