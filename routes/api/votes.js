49bcbdb715f5db7fefbceb973481e66438aafaf6
dancreppel <creppel@usc.edu>
2020-07-07 21:52:18 -0700

    Fix upvote and downvote

--- routes/api/votes.js
+++ routes/api/votes.js
@@ -4,10 +4,23 @@ const Vote = require("../../models/Vote");
 const passport = require("passport");
 
 // test
-router.get("/test", (req, res) => res.json({ msg: "This is the votes route" }));
+// router.get("/test", (req, res) => res.json({ msg: "This is the votes route" }));
 
-// creating test vote
-router.post('/test', (req, res) => Vote.create({ user: '123', track: '123'}).then(vote => res.json(vote)).catch(err => res.json(err)));
+// grab votes for currentUser
+router.get(
+  '/currentUser',
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Vote.find({
+      user: req.user.id
+    })
+      .then((vote) => {
+        return res.json(vote);
+      })
+      .catch((err) => {
+        return res.status(404).json(err);
+      });
+    });
 
 // votes index
 router.get('/', (req, res) => {
@@ -31,20 +44,50 @@ router.post(
   '/track/:track_id/upvote', 
   passport.authenticate("jwt", { session: false }), 
   (req, res) => {
-    Vote.find({ user: req.user.id, track: req.params.track_id })
+    Vote.findOne({ user: req.user.id, track: req.params.track_id })
       .then((vote) => {
-        // update vote since it exists
-        vote.rating = 1;
-        vote.save()
-          .then(vote => res.json(vote))
-          .catch(err => res.status(400).json(err))
+        if (vote) {
+          // update vote since it exists
+          vote.rating = 1;
+          vote.save()
+            .then(vote => res.json(vote))
+            .catch(err => res.status(400).json(err))
+        } else {
+          // create vote since it does not exist
+          Vote
+            .create({ user: req.user.id, track: req.params.track_id, rating: 1})
+            .then(vote => res.json(vote));
+        }
       })
-      .catch(() => {
-        // create vote since it does not exist
-        Vote
-          .create({ user: req.user.id, track: req.params.track_id, rating: 1})
-          .then(vote => res.json(vote));
-      });
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+// downvote track either create a vote instance if it does not already exist
+// OR update a vote instance // ! NOT RESTful
+router.post(
+  "/track/:track_id/downvote",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Vote.findOne({ user: req.user.id, track: req.params.track_id })
+      .then((vote) => {
+        if (vote) {
+          // update vote since it exists
+          vote.rating = -1;
+          vote
+            .save()
+            .then((vote) => res.json(vote))
+            .catch((err) => res.status(400).json(err));
+        } else {
+          // create vote since it does not exist
+          Vote.create({
+            user: req.user.id,
+            track: req.params.track_id,
+            rating: -1,
+          }).then((vote) => res.json(vote));
+        }
+      })
+      .catch((err) => res.status(404).json(err));
   }
 );
 
