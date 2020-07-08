d5b7180c72d74ffb0b24a3a008b94a7ebd861d88
Daniel Yee <danjamesyee@gmail.com>
2020-07-08 10:40:58 -0700

    fixes gitignore of keysdev

--- config/keys_dev.js
+++
@@ -1,9 +0,0 @@
-//connect database to MongoDB Atlas
-module.exports = {
-  mongoURI:
-    //this string from Atlas setup
-    "mongodb+srv://admin:dH3QCKNC3lqQymGv@cluster0.fmbgo.mongodb.net/MERNblocks?retryWrites=true&w=majority",
-
-  //add key-value pair to our object to use jsonwebtoken
-  secretOrKey: "l4Hc8OxJM3",
-};
