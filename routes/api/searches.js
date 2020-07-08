aea319558ef8a3e17cc72e56157a4b31e3bae9d1
dancreppel <creppel@usc.edu>
2020-07-08 11:32:43 -0700

    Temp commit

--- routes/api/searches.js
+++ routes/api/searches.js
@@ -2,8 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Track = require("../../models/Track");
 const User = require("../../models/User");
+const validText = require("../../validation/valid-text");
 
 // test
 router.get('/test', (req,res) => res.json("This is the search route"));
+
+// search
+router.get('/', (req,res) => {
+  if (!validText(req.body.query)) {
+    return res.status(400).json("Invalid Query");
+  }
+
+  const usersQuery = User.find({ handle: req.body });
+  const tracksQuery = Track.find({ title: req.body });
+
+  Promise.all([usersQuery, tracksQuery]);
+});
 
 module.exports = router;
