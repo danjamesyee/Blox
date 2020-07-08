e1e355eb573da2d386cd78d174cedd957c5e3da2
dancreppel <creppel@usc.edu>
2020-07-08 15:53:58 -0700

    Create comments actions

---
+++ frontend/src/actions/comments_actions.js
@@ -0,0 +1,54 @@
+import { 
+  getTrackComments, 
+  postComment, 
+  patchComment, 
+  deleteComment } from '../util/comments_api_util';
+
+export const RECEIVE_TRACK_COMMENTS = "RECEIVE_TRACK_COMMENTS";
+export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
+export const RECEIVE_COMMENT_ERRORS = "RECEIVE_COMMENT_ERRORS";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+
+const receiveTrackComments = (comments) => ({
+  type: RECEIVE_TRACK_COMMENTS,
+  comments
+});
+
+const receiveComment = (comment) => ({
+  type: RECEIVE_COMMENT,
+  comment
+});
+
+const receiveCommentErrors = (errors) => ({
+  type: RECEIVE_COMMENT_ERRORS,
+  errors
+});
+
+const removeComment = (commentId) => ({
+  type: REMOVE_COMMENT,
+  commentId
+});
+
+export const fetchTrackComments = (trackId) => dispatch => (
+  getTrackComments(trackId)
+    .then(comments => dispatch(receiveTrackComments(comments)))
+    .catch(err => dispatch(receiveCommentErrors(err.response.data)))
+);
+
+export const createComment = (comment) => dispatch => (
+  postComment(comment)
+    .then(createdComment => dispatch(receiveComment(createdComment)))
+    .catch(err => dispatch(receiveCommentErrors(err.response.data)))
+);
+
+export const updateComment = (comment) => dispatch => (
+  patchComment(comment)
+    .then(updatedComment => dispatch(receiveComment(updatedComment)))
+    .catch(err => dispatch(receiveCommentErrors(err.response.data)))
+);
+
+export const destroyComment = (commentId) => dispatch => (
+  deleteComment(commentId)
+    .then(() => dispatch(removeComment(commentId)))
+    .catch(err => dispatch(receiveCommentErrors(err.response.data)))
+);
