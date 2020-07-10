35cfc92b7e2d948b765a7cbe45d761eba7e7849e
dancreppel <creppel@usc.edu>
2020-07-10 11:01:21 -0700

    Add clear search action

--- frontend/src/actions/search_actions.js
+++ frontend/src/actions/search_actions.js
@@ -2,6 +2,7 @@ import { searchRequest } from '../util/search_api_util';
 
 export const RECEIVE_SEARCH = "RECEIVE_SEARCH";
 export const RECEIVE_SEARCH_ERRORS = "RECEIVE_SEARCH_ERRORS"
+export const CLEAR_SEARCH = "CLEAR_SEARCH"
 
 const receiveSearch = (searchResults) => ({
   type: RECEIVE_SEARCH,
@@ -11,6 +12,10 @@ const receiveSearch = (searchResults) => ({
 const receiveSearchErrors = errors => ({
   type: RECEIVE_SEARCH_ERRORS,
   errors
+});
+
+export const clearSearch = () => ({
+  type: CLEAR_SEARCH
 });
 
 export const fetchSearch = query => dispatch => {
