06ea3587019b61e50efc2ffb8694f0fbc66c5687
dancreppel <creppel@usc.edu>
2020-07-10 11:03:10 -0700

    Allow clear searched tracks

--- frontend/src/reducers/tracks_reducer.js
+++ frontend/src/reducers/tracks_reducer.js
@@ -7,7 +7,8 @@ import {
 } from "../actions/track_actions";
 
 import { 
-  RECEIVE_SEARCH
+  RECEIVE_SEARCH,
+  CLEAR_SEARCH
 } from "../actions/search_actions";
 
 const TracksReducer = (state = {}, action) => {
@@ -33,8 +34,12 @@ const TracksReducer = (state = {}, action) => {
       delete newState[action.trackId.data];
       return newState;
 
+    // search cases
     case RECEIVE_SEARCH: 
       return action.searchResults.tracks
+
+    case CLEAR_SEARCH:
+      return {};
 
     default:
       return state;
