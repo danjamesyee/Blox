4cb0875f3916dc99d672ef25f7d34f3d0cb3a69e
dancreppel <creppel@usc.edu>
2020-07-09 21:47:57 -0700

    Add users reducer to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -5,6 +5,7 @@ import blocks from "./blocks_reducer";
 import tracks from "./tracks_reducer";
 import comments from "./comments_reducer";
 import votes from "./votes_reducer";
+import users from "./users_reducer";
 
 const RootReducer = combineReducers({
   session,
@@ -12,7 +13,8 @@ const RootReducer = combineReducers({
   blocks,
   tracks,
   comments,
-  votes
+  votes,
+  users
 });
 
 export default RootReducer;
